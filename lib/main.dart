d19e3812dbfce16e2998e624ccbf2836839bd8a8
Jobjorn <672641@stud.hvl.no>
2025-04-29 14:12:36 +0200

    Initialize Supabase and load environment variables in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MyApp());
 }
+
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
